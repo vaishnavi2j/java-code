da9209b48c8add424f6c454f07b02a771fe4b184
vaishnavi <vaishnavijanjire0705@gamil.com>
2025-07-24 12:49:02 +0530

    this is my first commit

---
+++ square.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Square{
+public static void main (String args[])
+{
+int num=1;
+do{
+System.out.print("square of");
+System.out.println(num+":");
+System.out.println(num*num);
+num++;
+}while(num <=10);
+}
+}
+
