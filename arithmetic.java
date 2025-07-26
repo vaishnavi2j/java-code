1c54091d880ba3ab6fb4a66c87d0b21421337e8a
vaishnavi <vaishnavijanjire0705@gamil.com>
2025-07-26 12:15:41 +0530

    This is my commit

---
+++ arithmetic.java
@@ -0,0 +1,12 @@
+public class arithmetic
+{
+public static void main(String args[])
+{
+int a=20;
+int b=10;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+}
+}
